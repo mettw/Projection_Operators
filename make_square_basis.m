40228ecb8e08a13ba26e6283771ece896291b44d
Matthew Parry <matthew@mparry.id.au>
2023-02-08 11:25:17 +1100

    now handles arguments that are a mixture of odd and even numbers

--- make_square_basis.m
+++ make_square_basis.m
@@ -1,12 +1,17 @@
 function F = make_square_basis(kx_max, ky_max)
     %F = {};
-    if mod(kx_max,2) == 0 && mod(ky_max,2) == 0
-        [X,Y] = meshgrid(-kx_max+1:2:kx_max-1, ...
-            ky_max-1:-2:-ky_max+1);
+    if mod(kx_max,2) == 0 
+        kx_range = -kx_max+1:2:kx_max-1;
     else
-        [X,Y] = meshgrid(-(kx_max-1)/2:(kx_max-1)/2, ...
-            (ky_max-1)/2:-1:-(ky_max-1)/2);
+        kx_range = -(kx_max-1)/2:(kx_max-1)/2;
     end
+    if mod(ky_max,2) == 0
+        ky_range = ky_max-1:-2:-ky_max+1;
+    else
+        ky_range = (ky_max-1)/2:-1:-(ky_max-1)/2;
+    end
+    
+    [X,Y] = meshgrid(kx_range, ky_range);
 
     %for kx = 1:size(X,2)
     %    for ky = 1:size(Y,1)
