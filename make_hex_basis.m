6661a86ebbd4ad6ac712bf3c3dcae07c5f6c21c5
Matthew Parry <matthew@mparry.id.au>
2023-07-10 15:30:08 +1000

    bug fix

--- make_hex_basis.m
+++ make_hex_basis.m
@@ -1,25 +1,28 @@
 function [F, Kx, Ky, B] = make_hex_basis(m_max, varargin)
-    % Flat top or pointed top?
-    if nargin >= 2 && varargin{1} == true
-        b1 = [-1/2, sqrt(3)/2];
-        b2 = [1, 0];
-    else
-        b1 = [sqrt(3)/2, 1/2];
-        b2 = [sqrt(3)/2, -1/2];
-    end
-
+   
+    b1 = [sqrt(3)/2, 1/2];
+    b2 = [sqrt(3)/2, -1/2];
     [M,N] = meshgrid(-m_max:m_max, m_max:-1:-m_max);
 
-    Kx = M(:)*b1(1)+N(:)*b2(1);
-    Ky = M(:)*b1(2)+N(:)*b2(2);
-    F = [Kx(sqrt(Kx.^2+Ky.^2)<=m_max),Ky(sqrt(Kx.^2+Ky.^2)<=m_max)];
+    Kx = (M(:)*b1(1)+N(:)*b2(1))/m_max;
+    Ky = (M(:)*b1(2)+N(:)*b2(2))/m_max;
+    %F = [Kx(sqrt(Kx.^2+Ky.^2)<=m_max),Ky(sqrt(Kx.^2+Ky.^2)<=m_max)];
 
-    B = [b1.' b2.'];
 
-    % Scale all results down to a specified periodicity.
-    if nargin ==3
-        F = F/m_max*varargin{2};
-        Kx = Kx/m_max*varargin{2};
-        Ky = Ky/m_max*varargin{2};
+    F = [Kx(-b1(1)-1/m_max^2<=Kx & Kx<=b1(1)+1/m_max^2 & ...
+        -(b1(2)-b2(2))<=Ky & Ky<=(b1(2)-b2(2))), ...
+        Ky(-b1(1)-1/m_max^2<=Kx & Kx<=b1(1)+1/m_max^2 & ...
+        -(b1(2)-b2(2))<=Ky & Ky<=(b1(2)-b2(2)))];
+
+    % Flat top or pointed top?
+    if nargin == 2 && varargin{1} == true
+        b1 = [-1/2, sqrt(3)/2];
+        b2 = [1, 0];
+        F = F*[cosd(30) -sind(30); sind(30) cosd(30)];
+        K = [Kx Ky]*[cosd(30) -sind(30); sind(30) cosd(30)];
+        Kx = K(:,1);
+        Ky = K(:,2);
     end
+
+    B = [b1; b2];
 end
